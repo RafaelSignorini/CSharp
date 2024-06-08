3ab011d473f26c052f20b3198970ab49de036491
Senhor_Ini <rafabs26126@gmail.com>
2024-06-08 08:48:38 -0300

    exe05

--- CSharp/Básico/Exe05.cs
+++ CSharp/Básico/Exe05.cs
@@ -2,5 +2,17 @@
 
 namespace CSharp.Básico
 {
+    class Exe05
+    {
+        public static void Execute()
+        {
+            Console.WriteLine("Insira um número: ");
+            float num1 = float.Parse(Console.ReadLine());
 
+            Console.WriteLine("Insira mais um número: ");
+            float num2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+        }
+    }
 }
