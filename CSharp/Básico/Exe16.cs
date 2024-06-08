866b806e5df48ab44191d2e7cdc783f0a803b965
Senhor_Ini <rafabs26126@gmail.com>
2024-06-08 13:26:00 -0300

    Exe16

---
+++ CSharp/Básico/Exe16.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSharp.Básico
+{
+    class Exe16
+    {
+        public static void Execute()
+        {
+            // Par ou ímpar
+            Console.WriteLine("Insira um número para saber se é ímpar ou par: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine($"O número {num} é par.");
+            }
+            else
+            {
+                Console.WriteLine($"O número {num} é ímpar");
+            }
+        }
+    }
+}
--- CSharp/Básico/Program.cs
+++ CSharp/Básico/Program.cs
@@ -26,6 +26,7 @@ namespace CSharp.Básico
                 Console.WriteLine("13. Condições simples, nota");
                 Console.WriteLine("14. Jogo do adivinha");
                 Console.WriteLine("15. Leitor da velocidade de carros");
+                Console.WriteLine("16. Par ou ímpar");
                 Console.Write("Insira o número do exercício: ");
 
                 // Verifica se a entrada do usuário é um número inteiro válido
@@ -77,6 +78,9 @@ namespace CSharp.Básico
                             break;
                         case 15:
                             Exe15.Execute();
+                            break;
+                        case 16:
+                            Exe16.Execute();
                             break;
                         case 0:
                             Console.WriteLine("Saindo...");
