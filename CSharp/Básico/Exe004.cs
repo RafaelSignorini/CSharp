fb4802d2a1aa6910caeeb53dc3fd21ef8865995e
Senhor_Ini <rafabs26126@gmail.com>
2024-06-15 12:07:11 -0300

    esses exercícios são baseados nos exercícios do curso de python do curso em vídeo, por isso alguns deles, como este, não são tão básicos como sugere a pasta, mas isso é cortesia minha

--- CSharp/Básico/Exe004.cs
+++ CSharp/Básico/Exe004.cs
@@ -6,14 +6,78 @@ namespace CSharp.Básico
     {
         public static void Execute()
         {
-            // Somas básicas
-            Console.WriteLine("Insira um número: ");
-            float num1 = float.Parse(Console.ReadLine());
+            Console.Write("Insira alguma coisa: ");
+            string a = Console.ReadLine();
+            Console.WriteLine($"O tipo primitivo desse valor é {a.GetType()}");
+            Console.WriteLine($"Só tem espaços? {string.IsNullOrWhiteSpace(a)}");
+            Console.WriteLine($"É um número? {IsNumeric(a)}");
+            Console.WriteLine($"É alfabético? {IsAlpha(a)}");
+            Console.WriteLine($"É alfanumérico? {IsAlnum(a)}");
+            Console.WriteLine($"Está em maiúscula? {IsUpper(a)}");
+            Console.WriteLine($"Está em minúscula? {IsLower(a)}");
+            Console.WriteLine($"Está capitalizada? {IsTitle(a)}");
+        }
+        static bool IsNumeric(string str)
+        {
+            return int.TryParse(str, out _);
+        }
+        static bool IsAlpha(string str)
+        {
+            foreach (char c in str)
+            {
+                if (!char.IsLetterOrDigit(c))
+                    return false;
+            }
+            return true;
+        }
+        static bool IsAlnum(string str)
+        {
+            foreach (char c in str)
+            {
+                if (!char.IsLetterOrDigit(c))
+                    return false;
+            }
+            return true;
+        }
+        static bool IsUpper(string str)
+        {
+            foreach (char c in str)
+            {
+                if (char.IsLetter(c) && !char.IsUpper(c))
+                    return false;
+            }
+            return true;
+        }
+        static bool IsLower(string str)
+        {
+            foreach (char c in str)
+            {
+                if (char.IsLetter(c) && !char.IsLower(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static bool IsTitle(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
 
-            Console.WriteLine("Insira mais um número: ");
-            float num2 = float.Parse(Console.ReadLine());
-
-            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+            string[] words = str.Split(' ');
+            foreach (string word in words)
+            {
+                if (word.Length == 0)
+                    continue;
+                if (!char.IsUpper(word[0]))
+                    return false;
+                for (int i = 1; i < word.Length; i++)
+                {
+                    if (!char.IsLower(word[i]))
+                        return false;
+                }
+            }
+            return true;
         }
     }
 }
