8006557357ddae1b1cffec3e42f9771ed7a21caf
Senhor_Ini <rafabs26126@gmail.com>
2024-06-14 22:24:50 -0300

    deixei o código mais sussinto

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -12,28 +12,28 @@ namespace CSharp
             do
             {
                 Console.WriteLine("Escolha um exercício para executar: ");
-                Console.WriteLine("1. Básico");
-                Console.WriteLine("2. Variáveis interativas");
-                Console.WriteLine("3. Scripts");
-                Console.WriteLine("4. Somas básicas");
-                Console.WriteLine("5. Média aritmética");
-                Console.WriteLine("6. Tabuada");
-                Console.WriteLine("7. Pintando a parede");
-                Console.WriteLine("8. Calculando descontos");
-                Console.WriteLine("9. Reajuste salarial");
-                Console.WriteLine("10. Parcelamento e porcentagem");
-                Console.WriteLine("11. Aluguel de carros");
-                Console.WriteLine("12. Condições simples, nome");
-                Console.WriteLine("13. Condições simples, nota");
-                Console.WriteLine("14. Jogo do adivinha");
-                Console.WriteLine("15. Leitor da velocidade de carros");
-                Console.WriteLine("16. Par ou ímpar");
-                Console.WriteLine("17. Viagens longas");
-                Console.WriteLine("18. Ano bissexto");
-                Console.WriteLine("19. Maior e menor número");
-                Console.WriteLine("20. Aumento de salários");
-                Console.WriteLine("21. Retas de um triângulo");
-                Console.WriteLine("22. Conversor de medidas");
+                Console.WriteLine("1.");
+                Console.WriteLine("2.");
+                Console.WriteLine("3.");
+                Console.WriteLine("4.");
+                Console.WriteLine("5.");
+                Console.WriteLine("6.");
+                Console.WriteLine("7.");
+                Console.WriteLine("8.");
+                Console.WriteLine("9.");
+                Console.WriteLine("10.");
+                Console.WriteLine("11.");
+                Console.WriteLine("12.");
+                Console.WriteLine("13.");
+                Console.WriteLine("14.");
+                Console.WriteLine("15.");
+                Console.WriteLine("16.");
+                Console.WriteLine("17.");
+                Console.WriteLine("18.");
+                Console.WriteLine("19.");
+                Console.WriteLine("20.");
+                Console.WriteLine("21.");
+                Console.WriteLine("22.");
                 Console.Write("Insira o número do exercício: ");
 
                 // Verifica se a entrada do usuário é um número inteiro válido
